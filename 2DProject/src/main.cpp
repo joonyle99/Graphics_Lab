3bc44dc46ffb8fd6847bf607fcfe48559ec28c41
joonyle99 <gongjunyeol@gmail.com>
2026-03-11 20:23:51 +0900

    feat: add 2DProject for basic 2D rendering practice

---
+++ 2DProject/src/main.cpp
@@ -0,0 +1,12 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	ofGLWindowSettings glSettings;
+	glSettings.setSize(1024, 768);
+	glSettings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+	glSettings.setGLVersion(4, 1);
+	ofCreateWindow(glSettings);
+	ofRunApp(new ofApp());
+}
